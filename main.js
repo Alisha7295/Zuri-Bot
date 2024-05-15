60a8a3f0d60d56d2333477dfd2dcd2e2d40eeaf6
Alisha7295 <168325700+Alisha7295@users.noreply.github.com>
2024-05-15 14:45:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { readdirSync, readFileSync, writeFileSync, existsSync, unlinkSync, rm } = require("fs-extra");'
+const { readdirSync, readFileSync, writeFileSync, existsSync, unlinkSync, rm } = require("fs-extra");
 const { join, resolve } = require("path");
 const { execSync } = require('child_process');
 const logger = require("./utils/log.js");
